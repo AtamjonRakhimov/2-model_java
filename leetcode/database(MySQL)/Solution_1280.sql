2f812b64d6ac66fbcc8d401522d7edbef2270a15
AtamjonRaximov <raximovatam999@gmail.com>
2026-03-20 10:17:58 +0500

    next solution 1280

---
+++ leetcode/database(MySQL)/Solution_1280.sql
@@ -0,0 +1,7 @@
+select s.student_id, s.student_name,sb.subject_name, count(e.subject_name) as attended_exams 
+from students s
+cross join Subjects sb
+left join examinations e
+	on s.student_id =e.student_id and  sb.subject_name = e.subject_name
+group by s.student_id,s.student_name,sb.subject_name
+order by s.student_id,s.student_name
